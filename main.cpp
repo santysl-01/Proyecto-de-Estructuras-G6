9ec6ed4eb950b819ffcaee7ea8176aa66daacf73
santysl-01 <ms24017@ues.edu.sv>
2025-11-10 18:53:34 -0600

    Declarar Main

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+// todo bien
 #include <iostream>
 #include <vector>
 using namespace std;
+int main()
+{
+    vector<int> numeros = {1, 2, 3, 4, 5};
+    for (int num : numeros)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
