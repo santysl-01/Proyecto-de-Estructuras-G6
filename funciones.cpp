842974100b2c0e8b62e26d5e6a8a0eb2969bf111
santysl-01 <ms24017@ues.edu.sv>
2025-11-11 19:11:15 -0600

    add push, pop

--- funciones.cpp
+++ funciones.cpp
@@ -1,9 +1,27 @@
 #include "funciones.hpp"
 
-bool Pila::top(int &dato) const {
-    if (cuantos == 0) {
-        return false;  // pila vacía
-    }
-    dato = datos[cuantos - 1];  // copia el último elemento
-    return true;  // había elemento
+Pila::Pila() : cuantos(0) {}
+
+bool Pila::push(int dato)
+{
+    if (cuantos == capacidad)
+        return false; // pila llena
+    datos[cuantos++] = dato;
+    return true;
+}
+
+bool Pila::pop(int &dato)
+{
+    if (cuantos == 0)
+        return false; // pila vacía
+    dato = datos[--cuantos];
+    return true;
+}
+
+bool Pila::top(int &dato) const
+{
+    if (cuantos == 0)
+        return false;          // pila vacía
+    dato = datos[cuantos - 1]; // copia el último elemento
+    return true;               // había elemento
 }
